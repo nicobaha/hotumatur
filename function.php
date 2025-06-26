5cfde1ec22d971ceeeb3fdf5ddefaf4e1d2f1a17
Nico B <ni.bahamonde@duocuc.cl>
2025-06-25 20:12:36 -0400

    Ocultar precio en categoria tour especiales

--- function.php
+++ function.php
@@ -11,3 +11,12 @@ add_action('wp_enqueue_scripts', 'woodmart_child_enqueue_styles', 10010);
 
 // Ocultar el precio en la categoría 'tour-especiales'
 add_filter('woocommerce_get_price_html', 'ocultar_precio_categoria_especifica', 10, 2);
+
+function ocultar_precio_categoria_especifica($precio, $producto)
+{
+    // Cambia 'tour-especiales' por el slug real si usas otro
+    if (has_term('tour-especiales', 'product_cat', $producto->get_id())) {
+        return ''; // Retorna vacío para ocultar el precio
+    }
+    return $precio; // Muestra el precio normalmente si no está en esa categoría
+}
